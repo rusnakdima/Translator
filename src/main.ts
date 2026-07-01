3161d5ef787c175d38d9b42b89c0269a5c786599
Dmitriy303 <rusnakdima03@gmail.com>
2026-07-02 00:25:49 +0500

    chore: update main entry and tsconfig

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { App } from "./app/app";
 import { appConfig } from "./app/app.config";
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, appConfig)
+  .then(() => {
+    import("@tauri-front/shared");
+  })
+  .catch((err) => console.error(err));
