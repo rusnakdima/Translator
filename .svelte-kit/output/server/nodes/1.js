09682eb1f57b6a9e35235ced05baaebbcc948145
Dmitriy303 <rusnakdima03@gmail.com>
2026-07-28 23:01:17 +0500

    chore(Translator): add build output to gitignore

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.Dl7dN-7t.js","_app/immutable/chunks/m9j-v7T7.js","_app/immutable/chunks/lv0S4uG3.js","_app/immutable/chunks/-aTCarWr.js","_app/immutable/chunks/cD7LuWJT.js","_app/immutable/chunks/2YQ288tR.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/settings/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.CUCfxwOw.js","_app/immutable/chunks/m9j-v7T7.js","_app/immutable/chunks/lv0S4uG3.js","_app/immutable/chunks/No9K1P6r.js","_app/immutable/chunks/2YQ288tR.js","_app/immutable/chunks/cD7LuWJT.js"];
-export const stylesheets = [];
-export const fonts = [];
